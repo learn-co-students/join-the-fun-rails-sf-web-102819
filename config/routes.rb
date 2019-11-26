439810b694b3487ba7fc91e00f7d0489499df4c4
aryaziai <arya721@gmail.com>
2019-11-26 10:18:15 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :passengers #dry up later
+  resources :rides #dry up later
+  resources :taxies #dry up later
 end
